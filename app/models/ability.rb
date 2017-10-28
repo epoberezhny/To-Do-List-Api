e77eee6b6f9967d80616d03dd528f72f0ee83b93
Eugene <boberr10@yandex.ru>
2017-10-28 20:44:12 +0300

    ability is done

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,6 +4,8 @@ class Ability
   def initialize(user)
     return unless user
 
-    can :manage, Project, user: user
+    can :manage, Project, user_id: user.id
+    can :manage, Task,    project: { user_id: user.id }
+    can :manage, Comment, task:    { project: { user_id: user.id } }
   end
 end
