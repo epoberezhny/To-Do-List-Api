68c3f7d077109c3944754b380e97417c273dd765
Eugene <boberr10@yandex.ru>
2017-11-15 16:37:22 +0200

    fix comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,7 @@ class Comment < ApplicationRecord
   mount_uploader :attachment, AttachmentUploader
 
   belongs_to :task, counter_cache: true
+
+  validates :text, presence: true, if: -> { attachment.blank? }
+  validates :attachment, presence: true, if: -> { text.blank? }
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -5,11 +5,21 @@ RSpec.describe Comment, type: :model do
     end
   end
 
-  # context 'validations' do
-  #   context 'presence' do
-  #     it { is_expected.to validate_presence_of(:text) }      
-  #   end
-  # end
+  context 'validations' do
+    context 'presence' do
+      context 'text blank' do
+        subject(:comment) { build_stubbed(:comment, text: nil) }
+
+        it { is_expected.to validate_presence_of(:attachment) }
+      end
+
+      context 'attachment blank' do
+        subject(:comment) { build_stubbed(:comment, attachment: nil) }
+        
+        it { is_expected.to validate_presence_of(:text) }
+      end 
+    end
+  end
 
   context 'columns' do
     it { is_expected.to have_db_column(:attachment) }
